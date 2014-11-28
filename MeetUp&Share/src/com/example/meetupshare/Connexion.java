dd31b14f1434b3d8d046c2c2f84b4818fef77526
Romain <Romain@Choulo>
2014-11-28 10:14:33 +0100

    Modification page accueil (lien vers page inscription), creation page inscription

--- MeetUp&Share/src/com/example/meetupshare/Connexion.java
+++ MeetUp&Share/src/com/example/meetupshare/Connexion.java
@@ -1,12 +1,22 @@
 package com.example.meetupshare;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class Connexion extends Activity{
 	 @Override
 	    protected void onCreate(Bundle savedInstanceState) {
 	        super.onCreate(savedInstanceState);
-	        setContentView(R.layout.activity_main);
+	        setContentView(R.layout.connexion);
 	    }
+	 /**
+	 * Passage sur une autre activity
+	 */
+	 public void signIn(View view){
+	 
+	 Intent intent = new Intent(Connexion.this, SignIn.class);
+	 startActivity(intent);
+	 }
 }
