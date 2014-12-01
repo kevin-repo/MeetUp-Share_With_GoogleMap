448d84a752bec1f8b68a13f8a8028d2391cd2854
Romain <Romain@Choulo>
2014-12-01 15:00:29 +0100

    page d'inscription

---
+++ MeetUp&Share/src/com/example/meetupshare/SignIn.java
@@ -0,0 +1,13 @@
+package com.example.meetupshare;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class SignIn extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.signin);
+    }
+}
