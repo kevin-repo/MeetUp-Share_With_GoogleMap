3264d5f076b591d2291fd5f393e2fbe28498b459
Quentin Bagot <bagotq@gmail.com>
2014-12-01 15:17:20 +0100

    Toutes vues fini

--- MeetUp&Share/gen/com/example/meetupshare/R.java
+++ MeetUp&Share/gen/com/example/meetupshare/R.java
@@ -20,38 +20,48 @@ public final class R {
          */
         public static final int activity_horizontal_margin=0x7f040000;
         public static final int activity_vertical_margin=0x7f040001;
+        public static final int vinght=0x7f040002;
     }
     public static final class drawable {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f080013;
-        public static final int boite=0x7f08000c;
-        public static final int button1=0x7f080011;
-        public static final int button2=0x7f080009;
-        public static final int button3=0x7f08000e;
-        public static final int button4=0x7f08000d;
-        public static final int button5=0x7f08000f;
-        public static final int connexion=0x7f080002;
-        public static final int date=0x7f08000a;
-        public static final int description=0x7f080008;
-        public static final int editText1=0x7f080000;
-        public static final int editText2=0x7f080001;
-        public static final int editText3=0x7f080012;
-        public static final int heure=0x7f080007;
-        public static final int inscription=0x7f080003;
-        public static final int intitule=0x7f080004;
-        public static final int lieu=0x7f080005;
-        public static final int photos=0x7f08000b;
-        public static final int textView1=0x7f080010;
-        public static final int titre=0x7f080006;
+        public static final int LinearLayout1=0x7f080014;
+        public static final int action_settings=0x7f080019;
+        public static final int add=0x7f080007;
+        public static final int button1=0x7f080018;
+        public static final int button2=0x7f08000f;
+        public static final int button3=0x7f080012;
+        public static final int button4=0x7f080011;
+        public static final int button5=0x7f080013;
+        public static final int connexion=0x7f080004;
+        public static final int date=0x7f080010;
+        public static final int datePicker1=0x7f080015;
+        public static final int description=0x7f08000e;
+        public static final int editText1=0x7f080002;
+        public static final int editText2=0x7f080003;
+        public static final int editText3=0x7f080017;
+        public static final int expandableListView1=0x7f080001;
+        public static final int heure=0x7f08000c;
+        public static final int inscription=0x7f080005;
+        public static final int intitule=0x7f080009;
+        public static final int lieu=0x7f08000a;
+        public static final int liste_contacts=0x7f080006;
+        public static final int photos=0x7f08000d;
+        public static final int remove=0x7f080008;
+        public static final int textView1=0x7f080000;
+        public static final int timePicker1=0x7f080016;
+        public static final int titre=0x7f08000b;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
-        public static final int connexion=0x7f030001;
-        public static final int evenement=0x7f030002;
-        public static final int menu=0x7f030003;
-        public static final int signin=0x7f030004;
+        public static final int calandar=0x7f030001;
+        public static final int connexion=0x7f030002;
+        public static final int contacts=0x7f030003;
+        public static final int evenement=0x7f030004;
+        public static final int menu=0x7f030005;
+        public static final int nouvel_evenement=0x7f030006;
+        public static final int signin=0x7f030007;
     }
     public static final class menu {
         public static final int main=0x7f070000;
