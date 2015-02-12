41eba20b87716a9c9816a110640a332b403e89f1
Romain <Romain@SuperRey>
2015-02-12 10:56:46 +0100

    menu

--- MeetUp&Share/src/com/example/meetupshare/MainActivity.java
+++ MeetUp&Share/src/com/example/meetupshare/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.meetupshare;
 
+import com.example.models.User;
+
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -8,29 +11,75 @@ import android.view.MenuItem;
 
 public class MainActivity extends Activity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-    }
+	User currentUser;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+	}
 
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
-    }
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
-    }
+	public boolean onOptionsItemSelected(MenuItem item) {
+		// Handle item selection
+		switch (item.getItemId()) {
+
+		case R.id.home:
+			Intent intent = new Intent(this, Home.class);
+			Bundle bundle = new Bundle();
+			bundle.putSerializable("currentUser", currentUser);
+			intent.putExtras(bundle);
+			startActivity(intent);
+			return true;
+		case R.id.options:
+			Intent intent1 = new Intent(this, Options.class);
+			Bundle bundle1 = new Bundle();
+			bundle1.putSerializable("currentUser", currentUser);
+			intent1.putExtras(bundle1);
+			startActivity(intent1);
+			return true;
+		case R.id.contact:
+			Intent intent2 = new Intent(this, Contacts.class);
+			Bundle bundle2 = new Bundle();
+			bundle2.putSerializable("currentUser", currentUser);
+			intent2.putExtras(bundle2);
+			startActivity(intent2);
+			return true;
+			//marchepas
+		case R.id.calandar:
+			Intent intent3 = new Intent(this, Calandar.class);
+			Bundle bundle3 = new Bundle();
+			bundle3.putSerializable("currentUser", currentUser);
+			intent3.putExtras(bundle3);
+			startActivity(intent3);
+			return true;
+		case R.id.newevent:
+			Intent intent4 = new Intent(this, Nouvel_evenement.class);
+			Bundle bundle4 = new Bundle();
+			bundle4.putSerializable("currentUser", currentUser);
+			intent4.putExtras(bundle4);
+			startActivity(intent4);
+			return true;
+		case R.id.photos:
+			Intent intent5 = new Intent(this, SeePics.class);
+			Bundle bundle5 = new Bundle();
+			bundle5.putSerializable("currentUser", currentUser);
+			intent5.putExtras(bundle5);
+			startActivity(intent5);
+			return true;
+			
+
+
+
+		default:
+			return super.onOptionsItemSelected(item);
+		}
+	}
 }
